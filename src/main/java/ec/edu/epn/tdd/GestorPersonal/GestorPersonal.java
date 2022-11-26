06c58c465a5ab4233d4b78e229f5712f5fe9f6a2
Steven Quispe <steven.quispe@epn.edu.ec>
2022-11-26 18:12:30 -0500

    Test Unitario 1:  given_technician_name_when_check_existence_then_ok

--- src/main/java/ec/edu/epn/tdd/GestorPersonal/GestorPersonal.java
+++ src/main/java/ec/edu/epn/tdd/GestorPersonal/GestorPersonal.java
@@ -1,7 +1,105 @@
 package ec.edu.epn.tdd.GestorPersonal;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class GestorPersonal {
-    public int add(int a, int b){
-        return a+b;
+    //Atributos
+    private Tecnico tecnico;
+    private String nombreTecnico;
+    private int numeroTecnico;
+
+    //Scanner para lectura de datos
+    Scanner lectura = new Scanner (System.in);
+
+    //Métodos
+
+    //Registro Personal
+    public void RegistrarTecnico(){
+        //Ingreso de datos del nuevo Tecnico
+        System.out.println("\t Registrar Tecnico ");
+        System.out.println("Ingrese el apellido del técnico:");
+        nombreTecnico = lectura.next();
+        System.out.println("Ingrese el nombre del técnico:");
+        nombreTecnico += lectura.next();
+        System.out.println("Ingrese el numero telefonico del técnico:");
+        numeroTecnico = lectura.nextInt();
+        tecnico = new Tecnico(nombreTecnico.toUpperCase(), numeroTecnico); //Registro del tecnico
+    }
+
+    //Eliminar Tecnico
+    public void EliminarTecnico(String nombreTecnico){
+        try {
+            File archivo = new File("C:\\Users\\Steven\\Desktop\\PersonalConcesionario\\"
+                    + nombreTecnico.toUpperCase() +".txt");
+            if (archivo.delete()) {
+                System.out.println("Tecnico eliminado Correctamente");
+            }
+        } catch (Exception e) {
+            System.out.println("Error al eliminar");
+
+        }
+    }
+
+    //Metodos para modificar
+    public boolean comprobarExistenciaTecnico(String nombreTecnico){
+        File archivo;
+        FileReader fr;
+        BufferedReader br;
+
+        try {
+            archivo = new File("C:\\Users\\Steven\\Desktop\\PersonalConcesionario\\" +
+                    nombreTecnico.toUpperCase() +".txt");
+            if (archivo.exists()) {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("No se encontró registros para la cita especificada");
+        }
+        return false;
+    }
+
+
+    //Menu
+    public void Menu(){
+        int opcion=0;
+        while (opcion != 3) {
+
+            System.out.println("---------------------------------------------------------");
+            System.out.println("\t Concesionario de Autos - Gestor Personal" +
+                    "\n  1.Registrar Tecnico" +
+                    "\n  2.Eliminar Tecnico"+
+                    "\n  3.Salir")
+            ;
+
+            Scanner lectura = new Scanner (System.in);
+            System.out.print("Ingrese la opcion requerida: ");
+            opcion = lectura.nextInt();
+            System.out.println("---------------------------------------------------------");
+
+
+            switch (opcion){
+                case 1:                //Registro Tecnico
+                    RegistrarTecnico();
+                    tecnico.RegistrarRespaldo();
+                    break;
+                case 2:  //Eliminar Tecnico
+                    System.out.println("Eliminar Tecnico ");
+                    System.out.println("Ingrese el apellido del técnico a eliminar:");
+                    nombreTecnico = lectura.next();
+                    System.out.println("Ingrese el nombre del técnico a eliminar:");
+                    nombreTecnico += lectura.next();
+                    EliminarTecnico(nombreTecnico);
+                    break;
+                case 3:  //Salir
+                    break;
+                default:
+                    System.out.print("Opcion no valida");
+                    break;
+            }
+        }
+
     }
 }
--- src/test/java/ec/edu/epn/tdd/GestorPersonal/GestorPersonalTest.java
+++ src/test/java/ec/edu/epn/tdd/GestorPersonal/GestorPersonalTest.java
@@ -6,12 +6,11 @@ import static org.junit.Assert.*;
 
 public class GestorPersonalTest {
 
+    //Test to Develop Modify Technician Data
     @Test
-    public void given_when_them(){
+    public void given_technician_name_when_check_existence_then_ok(){
         GestorPersonal g1 = new GestorPersonal();
-        int expected = 6;
-        int actual = g1.add(4,2);
-        assertEquals(expected, actual);
+        assertTrue(g1.comprobarExistenciaTecnico("PerezJuan"));
     }
 
 }
